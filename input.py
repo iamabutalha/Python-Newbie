c4854aa058b885eb94e6b0f49e51fd17af5be527
iamabutalha <maabutalhakhalil@gmail.com>
2024-07-27 12:58:33 +0500

    Newbie Input()

---
+++ input.py
@@ -0,0 +1,14 @@
+a = input()
+print(a)
+
+a = input("Enter Your Name ")
+print("My name is: ", a)
+
+name = "AbuTalha"
+friend = "Khalil"
+other_friend = "shuja"
+print("Hello," + name)
+
+apple = "He said,\"He want to eat apple \" "
+print(apple)
+
