ce1defea2eca1ece41cffc08dcea8c002c562518
iamabutalha <maabutalhakhalil@gmail.com>
2024-07-27 12:51:28 +0500

    Arthematic operations

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+a = int(input("Enter a number : "))
+b = int(input("Enter another number "))
+
+subtraction = a-b
+addition = a+b
+multiplication = a * b
+division = a/b
+power = a ** b
+
+print(subtraction)
+print(addition)
+print(multiplication)
+print(division)
+print(power)
