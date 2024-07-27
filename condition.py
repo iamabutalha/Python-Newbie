c4ebabbdb87e5b0144bed7676d8abd53b18dff17
iamabutalha <maabutalhakhalil@gmail.com>
2024-07-27 12:53:16 +0500

    comparision operators

---
+++ condition.py
@@ -0,0 +1,21 @@
+Age = int(input("Enter Your Age : "))
+
+# if(int(Age) > 18 ):
+#     print("You are eligable to drive ")
+# elif(int(Age) < 18 ):
+#     print("You are not eligable to drive ")
+
+
+# print("Yaay!!!!")
+
+print(f"Your Age is {Age}")
+
+print(Age == 18)
+print(Age < 18)
+print(Age > 18)
+print(Age >= 18)
+print(Age <= 18)
+print(Age != 18)
+
+
+
