c36f85756ef08d225e5c8d2992dec256dd23baa6
iamabutalha <maabutalhakhalil@gmail.com>
2024-07-27 12:50:15 +0500

    Understanding array

---
+++ array.py
@@ -0,0 +1,22 @@
+Name = "Abutalha"
+
+print(Name[0])
+print(Name[1])
+print(Name[2])
+print(Name[3])
+print(Name[4])
+print(Name[5])
+print(Name[6])
+
+print(Name[7])
+
+
+for character in Name:
+    print(character)
+
+
+length = len(Name)
+
+print(f"The length of {Name} is {length}")
+
+
